ebc47ad8cd817aa61b9a02341c4cf52b243ad54c
Roos de Bildt <roosdebildt@gmail.com>
2015-08-15 16:19:18 +0200

    adds twitterfeed to about page

--- blog/app/controllers/pages_controller.rb
+++ blog/app/controllers/pages_controller.rb
@@ -4,8 +4,19 @@ class PagesController < ApplicationController
   end
 
   def about
+    @tweets = twitter_client.user_timeline("team_cheesy")
   end
 
   def contact
   end
+
+  private
+    def twitter_client
+      @twitter_client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key        = "secret"
+        config.consumer_secret     = "secret"
+        config.access_token        = "secret"
+        config.access_token_secret = "secret"
+      end
+    end
 end
